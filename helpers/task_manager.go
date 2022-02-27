4a32dd37b8a7c31c05950d28f04e7be43df09ba7
bryce-soghigian <bsoghigian@gmail.com>
2022-02-27 14:42:29 -0800

    sheeeeesh

--- helpers/task_manager.go
+++ helpers/task_manager.go
@@ -1,10 +1,15 @@
 package helpers
 
+import (
+	"fmt"
+	"github.com/Bryce-Soghigian/bry-task-cli/persistence"
+)
 func AddTask(command_args []string) (response string, err error) 
-	CreateNewTask(command_args[0], command_args[1])
+	persistence.CreateNewTask(command_args[1], command_args[3])
 	return "Successfully Added Task.", nil
 }
 func RemoveTask(command_args []string) (response string, err error) {
+	fmt.Println(command_args)
 	return "Successfully Removed Task.", nil
 }
 func ModifyTask(command_args []string) (response string, err error) {
